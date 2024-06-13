621b9e19358b815410c0f3e3f8b070d3733d420d
nosedhd <jose.cruz1@udea.edu.co>
2024-06-13 17:01:20 -0500

    Controladores de register, login, configuración de api y configuracióon de session

--- app/Http/Controllers/GlobalScalesController.php
+++ app/Http/Controllers/GlobalScalesController.php
@@ -8,29 +8,28 @@ use \App\Models\Note;
 
 class GlobalScalesController extends Controller
 {
-    function getScales (){
+    public function getScales (){
         $scales = new GlobalScale();
         $scale = $scales -> getScales();
         return $scale;
     }
 
-    function getScaleById($id){
+    public function getScaleById($id){
         $scales = new GlobalScale();
         $scale = $scales -> getScaleById($id);
         return $scale;
     }
     
-    function getIntervalsScaleById($id){
+    public function getIntervalsScaleById($id){
         $scales = new GlobalScale();
         $scale = $scales -> getIntervalsScaleById($id);
         return $scale;
     }
-    public function handleChordIndex(Request $request){
+
+    public function handleChordIndex($chordIndex, $scaleId){
+        // var_dump($scaleId);die;
         $notesChord=[];
-        // Traemos el index de la nota desde el front
-        $chordIndex = $request->input('chordIndex');
-        // Traemos el index de la escala desde el front
-        $scale = getIntervalsScaleById($request->input('scaleId'));
+        $scale = getIntervalsScaleById($scaleId);
         // Creamos los objetos Note
         $firstNote = new Note();
         $secondNote = new Note();
@@ -45,6 +44,6 @@ class GlobalScalesController extends Controller
         array_push($notesChord, $thirdNote);
 
         
-        return response()->json(['notesChord' => $notesChord]);
+        return $notesChord;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NotesController;
 use App\Http\Controllers\LanguagesController;
 use App\Http\Controllers\GlobalScalesController;
-
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LoginController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -25,4 +26,8 @@ Route::get('/notas', [NotesController::class, 'getNotes']);
 Route::get('/idiomas', [LanguagesController::class, 'getLanguages'])->name('idiomas');
 Route::get('/scales', [GlobalScalesController::class, 'getScales']);
 Route::get('/getScaleNotes/{tonic}/{intervals}', [NotesController::class, 'getScalesNotes']);
-Route::post('/global-scales', 'GlobalScalesController@handleChordIndex');
+Route::get('/global-scales/{chordIndex}/{scaleId}', [GlobalScalesController::class, 'handleChordIndex']);
+// Route::post('/global-scales', 'GlobalScalesController@handleChordIndex');
+
+Route::post('/login', [LoginController::class, 'check']);
+Route::post('/register', [RegisterController::class, 'store']);
